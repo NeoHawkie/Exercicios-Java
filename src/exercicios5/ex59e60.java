0cf1fa99f7aef66618d3ec9f072b2687a8f458eb
Wallyson Martins de Lima <wallysonml13@gmail.com>
2024-08-12 22:01:13 -0300

    08122024

--- src/exercicios5/ex59e60.java
+++ src/exercicios5/ex59e60.java
@@ -5,6 +5,7 @@
 package exercicios5;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
@@ -19,10 +20,13 @@ public class ex59e60 {
         Scanner input3 = new Scanner(System.in);
 
         ArrayList<String> nome = new ArrayList<>();
+        ArrayList<String> tempString = new ArrayList<>();
         ArrayList<Character> sexo = new ArrayList<>();
+        int temp = 0;
         ArrayList<Integer> idade = new ArrayList<>();
-        ArrayList<Integer> temp = new ArrayList<>();
-        int i = 0, totM=0, mediaM;
+        ArrayList<Integer> tempInt = new ArrayList<>();
+        int i = 0, totM=0;
+        
         String continuar;
         
         System.out.println("--------- CADASTRO DE pissoas ---------");
@@ -52,34 +56,67 @@ public class ex59e60 {
         System.out.println("---------- CADASTRO COMPLETO ----------");
         
         //a1 e a2)
-        System.out.print("\nA maior idade cadastrada: ");
-        temp.addAll(idade);
-        temp.sort(null);
-        System.out.println(temp.getLast());
-        temp.clear();
+        System.out.print("\nA pessoa mais velha cadastrada: ");
+        tempInt.addAll(idade);
+        tempInt.sort(null);
+        for (int j = 0; j < tempInt.size(); j++) {
+            if (Objects.equals(tempInt.getLast(), idade.get(j))) {
+                temp = j;
+            }
+        }
+        System.out.println(nome.get(temp) + " com "+ tempInt.getLast() + " anos.");
+        tempInt.clear();
         
-        //b)
-        System.out.print("Quantos homens foram cadastrados: ");
+        //b1 e d2)
+        System.out.print("\nQuantos homens foram cadastrados: ");
         i=0;
+        temp=0;
         do {
-            if (sexo.get(i).equals('M')) {
+            if (sexo.get(i) == 'M') {
                 totM++;
+                if (idade.get(i) > 30) {
+                    temp++;
+                }
             }
             i++;
         } while (i < sexo.size());
-        System.out.println(totM);
+        System.out.print(totM);
+        if (temp != 0) {
+            System.out.println(", e " + temp + " tem mais de 30 anos.");
+        }
+        temp=0;        
         
-        //c)
-        System.out.println("A idade da mulher mais jovem: ");
+        //c1 e b2)
+        i=0;
+        System.out.print("\nA idade da mulher mais jovem: ");
         do {
-            if (sexo.get(i).equals('F')) {
-                temp.add(idade.get(i));
+            if (sexo.get(i) == 'F') {
+                tempInt.add(idade.get(i));
+                tempString.add(nome.get(i));
             }
             i++;
-        } while (i < sexo.size());
-        temp.sort(null);
-        System.out.println(temp.getFirst());
-        temp.clear();
+        } while (sexo.size()-1 < i);
+        i=0;
+        int menor = tempInt.get(0), index=0;
+        for (int j = 1; j < tempInt.size()-1; j++) {
+            if (menor < tempInt.get(j)) {
+                menor = tempInt.get(j);
+                index = j;
+            }
+        }
+        System.out.println(tempString.get(index) + " com " + menor + " anos.");
+        tempInt.clear();
+        tempString.clear();
+        
+//        do {
+//            if (sexo.get(i).equals('F')) {
+//                temp1.add(idade.get(i));
+//            }
+//            i++;
+//        } while (i < sexo.size());
+//        temp1.sort(null);
+//        System.out.println(temp1.getFirst());
+//        temp1.clear();
         
         
     }
